bccd5155c3e76f046d21058adab76f08c69efb82
Muhamad Apriansyah Ramadhan <37446681+iansyahr@users.noreply.github.com>
2024-11-13 22:51:50 +0700

    fix url redirect

--- vsctunnel/vscode_cli.py
+++ vsctunnel/vscode_cli.py
@@ -1,7 +1,6 @@
 from fastnanoid import generate
 import subprocess
 import os
-import time
 import re
 
 def install_binary():
@@ -11,8 +10,7 @@ def install_binary():
     except (subprocess.CalledProcessError, FileNotFoundError):
         print("VSCode CLI is not installed")
         print("Install VSCode CLI...")
-        # https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64
-        os.system("wget https://vscode.download.prss.microsoft.com/dbazure/download/stable/384ff7382de624fb94dbaf6da11977bba1ecd427/vscode_cli_alpine_x64_cli.tar.gz")
+        os.system("curl -L 'https://code.visualstudio.com/sha/download?build=stable&os=cli-alpine-x64' --output vscode_cli_alpine_x64_cli.tar.gz")
         os.system("tar -xf vscode_cli_alpine_x64_cli.tar.gz")
         os.environ['PATH'] += ":/content/"
         
